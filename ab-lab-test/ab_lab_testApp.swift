ce01f7c5ae9be78ed34ee66514f28ad6a0aca316
jaddy.shin <jaddy.shin@kakaocorp.com>
2025-06-04 17:45:47 +0900

    Initial Commit

---
+++ ab-lab-test/ab_lab_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ab_lab_testApp.swift
+//  ab-lab-test
+//
+//  Created by jaddy on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ab_lab_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ab-lab-testTests/ab_lab_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  ab_lab_testTests.swift
+//  ab-lab-testTests
+//
+//  Created by jaddy on 6/4/25.
+//
+
+import Testing
+@testable import ab_lab_test
+
+struct ab_lab_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
